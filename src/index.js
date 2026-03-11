7e1ea4b67d1291cc5d8897282d90ad040aefd814
utkarsh vijay <utkarshvijay.it26@jecrc.ac.in>
2026-03-11 17:06:04 +0530

    Setup Basic Dotenv

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const { PORT } = require("./config/serverConfig");
+
+
+const setupAndStartServer = async() => {
+
+    // create express object
+    const app=express();
+    const PORT=3000;
+    app.listen(PORT,()=> {
+        console.log(`Server start at ${PORT}`);
+    })
+}
+setupAndStartServer();

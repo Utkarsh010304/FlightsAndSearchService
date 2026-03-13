a64ba6e9c4dc4ad6755dfc045574377b22340c7d
utkarsh vijay <utkarshvijay.it26@jecrc.ac.in>
2026-03-13 20:53:16 +0530

    Added Airports model

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -44,9 +44,10 @@ class CityService{
             throw{error};
         }
     }
-    async getAllCities(){
+    async getAllCities(filter){
         try{
-            const cities=await this.cityRepository.getAllCities();
+            const cities=await this.cityRepository.getAllCities({name: filter.name});
+            return cities;
         }
         catch(error){
             console.log("Something went wong in the servics Layer");
